e3f3159b0d6a8b597682535c9701bd2f190355cf
Ryan Chipman <ryan@ryanchipman.com>
2017-11-03 12:12:49 -0400

    Revert "implement dummy breadcrumbs in ui"
    This reverts commit 98ade95e7b24257d16e00398b9208a92de730b26.
    The breadcrumbs were not responsive, so we're omitting them for now.

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -10,25 +10,10 @@
             );
             return m('header', { class: headerClasses },
                 m('.mdl-layout__header-row',
-                    m('span.mdl-layout-title', m(Breadcrumbs, {
-                        _path: ['Set 2', 'Table 1'],
-                    })),
-                    m('.mdl-layout-spacer'),
                     m('span.mdl-layout-title', 'Bridge Bidder'),
+                    m('.mdl-layout-spacer'),
                 ),
             );
-        },
-    };
-
-    Breadcrumbs = {
-        view: (vnode) => {
-            var path = vnode.attrs._path;
-            return [
-                m('a.mdl-button', 'Home'),
-                path.map(item => {
-                    return [ m('span', ' > '), m('a.mdl-button', item) ];
-                }),
-            ];
         },
     };
 
