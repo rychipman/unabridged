98ade95e7b24257d16e00398b9208a92de730b26
Ryan Chipman <ryan@ryanchipman.com>
2017-11-03 12:01:34 -0400

    implement dummy breadcrumbs in ui

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -10,10 +10,25 @@
             );
             return m('header', { class: headerClasses },
                 m('.mdl-layout__header-row',
-                    m('span.mdl-layout-title', 'Bridge Bidder'),
+                    m('span.mdl-layout-title', m(Breadcrumbs, {
+                        _path: ['Set 2', 'Table 1'],
+                    })),
                     m('.mdl-layout-spacer'),
+                    m('span.mdl-layout-title', 'Bridge Bidder'),
                 ),
             );
+        },
+    };
+
+    Breadcrumbs = {
+        view: (vnode) => {
+            var path = vnode.attrs._path;
+            return [
+                m('a.mdl-button', 'Home'),
+                path.map(item => {
+                    return [ m('span', ' > '), m('a.mdl-button', item) ];
+                }),
+            ];
         },
     };
 
